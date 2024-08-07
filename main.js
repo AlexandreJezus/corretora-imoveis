a234da8725c02fb63b26ac6cf10a505dd269060d
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-08-07 09:29:19 -0300

    crud+

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const prompt = require("prompt-sync")();
 const corretora = require("./modulos/corretora.js");
 const cliente = require("./modulos/cliente.js");
 const corretor = require("./modulos/corretor.js");
+const imovel = require("./modulos/imovel.js");
+const venda = require("./modulos/venda.js");
 
 const menuPrincipal = () => {
   console.log(`
@@ -16,6 +18,8 @@ const menuPrincipal = () => {
                   1 - CORRETORA
                   2 - CLIENTE
                   3 - CORRETOR
+                  4 - IMÓVEL
+                  5 - VENDA
                   0 - SAIR
                   `);
 
@@ -34,6 +38,14 @@ const menuPrincipal = () => {
         menuCorretor();
         break;
 
+      case 4:
+        menuImovel();
+        break;
+
+      case 5:
+        menuVenda();
+        break;
+
       case 0:
         process.exit();
         break;
@@ -45,7 +57,7 @@ const menuPrincipal = () => {
 };
 
 const menuCliente = () => {
-  console.log("GERENCIAMENTO DE CLIENTE");
+  console.log("GERENCIAMENTO DE imovel");
   while (true) {
     console.log(`
                 1 - CADASTRAR CLIENTE
@@ -141,6 +153,78 @@ const menuCorretora = () => {
         break;
       case 4:
         corretora.destroy();
+        break;
+
+      case 0:
+        return;
+      default:
+        console.log("OPCAO INVALIDA");
+        break;
+    }
+  }
+};
+
+const menuImovel = () => {
+  console.log("GERENCIAMENTO DE Imovel");
+  while (true) {
+    console.log(`
+                1 - CADASTRAR IMOVEL
+                2 - LISTAR IMOVEIS
+                3 - ATUALIZAR IMOVEL
+                4 - EXCLUIR IMOVEL
+                0 - SAIR
+                `);
+
+    const opcaoServico = parseInt(prompt(": "));
+
+    switch (opcaoServico) {
+      case 1:
+        imovel.store();
+        break;
+      case 2:
+        imovel.index();
+        break;
+      case 3:
+        imovel.update();
+        break;
+      case 4:
+        imovel.destroy();
+        break;
+
+      case 0:
+        return;
+      default:
+        console.log("OPCAO INVALIDA");
+        break;
+    }
+  }
+};
+
+const menuVenda = () => {
+  console.log("GERENCIAMENTO DE Imovel");
+  while (true) {
+    console.log(`
+                1 - CADASTRAR VENDA
+                2 - LISTAR VENDAS
+                3 - ATUALIZAR VENDA
+                4 - EXCLUIR VENDA
+                0 - SAIR
+                `);
+
+    const opcaoServico = parseInt(prompt(": "));
+
+    switch (opcaoServico) {
+      case 1:
+        venda.store();
+        break;
+      case 2:
+        venda.index();
+        break;
+      case 3:
+        venda.update();
+        break;
+      case 4:
+        venda.destroy();
         break;
 
       case 0:
---
+++ modulos/venda.js
@@ -0,0 +1,92 @@
+const prompt = require("prompt-sync")();
+
+const imovel = require("./imovel.js");
+const corretor = require("./corretor.js");
+const corretora = require("./corretora.js");
+const db = [];
+
+let proxId = 1;
+
+const model = (id = proxId++) => {
+  const nome = prompt("Nome: ");
+
+  let id_corretor = 0;
+  if (corretor.index()) {
+    id_corretor = parseInt(prompt("ID do corretor: "));
+  } else {
+    console.log("Cadastre um corretor para inserir uma venda.");
+  }
+
+  if (nome != "" && corretor.show(id_corretor)) {
+    return {
+      id,
+      nome,
+      id_corretor,
+    };
+  }
+  console.log("Dados inválidos");
+};
+
+const store = () => {
+  const novo = model();
+
+  if (novo) {
+    db.push(novo);
+
+    console.log("Registro concluido com sucesso!");
+  }
+};
+
+const index = () => {
+  if (db.length == 0) {
+    console.log("Nenhum registro encontrado.");
+    return false;
+  }
+
+  db.forEach((el) => console.log(el));
+  return true;
+};
+
+const show = (id) => db.find((el) => el.id == id);
+
+const update = () => {
+  if (index()) {
+    const id = parseInt(prompt("ID: "));
+
+    const indice = db.findIndex((el) => el.id == id);
+
+    if (indice != -1) {
+      const novo = model(id);
+
+      if (novo) {
+        db[indice] = novo;
+        console.log("Registro atualizado com sucesso.");
+      }
+    } else {
+      console.log("Registro não encontrado");
+    }
+  }
+};
+
+const destroy = () => {
+  if (index()) {
+    const id = parseInt(prompt("ID: "));
+
+    const indice = db.findIndex((el) => el.id == id);
+
+    if (indice != -1) {
+      db.splice(indice, 1);
+      console.log("Registro excluído com sucesso");
+    } else {
+      console.log("Registro não encontrado");
+    }
+  }
+};
+
+module.exports = {
+  store,
+  index,
+  show,
+  update,
+  destroy,
+};
